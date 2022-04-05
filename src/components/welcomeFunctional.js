f4776641a6222025bbb635870eeb0eef60241aa2
PrashantGuptaa <meengineerprashant@gmail.com>
2022-04-05 21:59:47 +0530

    First Class and Functional Component

---
+++ src/components/welcomeFunctional.js
@@ -0,0 +1,14 @@
+// Class Component
+// Functional Component
+
+const WelcomeFunctionalComponent = () => {
+  return (
+    <h1>
+      <div>
+        <p>Welcome Functional Component</p>
+      </div>
+    </h1>
+  );
+};
+
+export default WelcomeFunctionalComponent;
